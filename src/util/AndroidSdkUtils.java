82aad636551f52c3d6776c8d34a9ccbb2314ad16
Dale <outofthisworld24@hotmail.com>
2016-07-22 13:50:16 +1200

    commit

--- src/util/AndroidSdkUtils.java
+++ src/util/AndroidSdkUtils.java
@@ -6,11 +6,11 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import static util.GlobalConstants.FILE_SEPARATOR;
-import static util.GlobalConstants.separatedSystemPath;
+import static util.GlobalConstants.*;
 
 /**
  * Created by Dale on 21/07/16.
@@ -25,12 +25,15 @@ public final class AndroidSdkUtils {
     private static final String ADB_TOOL = "adb";
     public static final String ANDROID_SDK_ENV_VARIABLE = "ANDROID_SDK_DIR";
     private static final Charset CMD_OUTPUT_ENCODING = Charset.forName("UTF-8");
-
     private static final String CREATE_AVD = "create avd";
     private static final String LIST_AVD = "list avd";
 
 
     public static boolean createAVD(String sdkLocation, String name, String target, String abi) throws IOException {
+
+        if(sdkLocation == null && (sdkLocation = tryLocateAndroidSDK()) == null)
+            Objects.requireNonNull(sdkLocation);
+
 
         String avdNameOption = " --name  ";
         String avdTargetOption = " --target ";
@@ -55,7 +58,22 @@ public final class AndroidSdkUtils {
         return ProcessUtils.executeCommand(command, new StringWriter(), output, CMD_OUTPUT_ENCODING);
     }
 
-    public static boolean tryCreateAVD(String name, String target, String abi) throws Exception {
+    public static String avdList(String sdkLocation) throws IOException {
+        StringBuilder commandBuilder = new StringBuilder();
+        String command = commandBuilder.append(sdkLocation)
+                .append(FILE_SEPARATOR)
+                .append(ANDROID_TOOLS_DIRECTORY)
+                .append(FILE_SEPARATOR)
+                .append(ANDROID_TOOL)
+                .append(SPACE)
+                .append(LIST_AVD).toString();
+
+        StringWriter sw = new StringWriter();
+        ProcessUtils.executeCommand(command,sw, null, CMD_OUTPUT_ENCODING);
+        return sw.getBuffer().toString();
+    }
+
+    public static boolean createAVD(String name, String target, String abi) throws Exception {
         String androidSdkLocation;
         if((androidSdkLocation = tryLocateAndroidSDK()) == null)
             throw new Exception("Cannot locate android sdk to create android virtual device");
