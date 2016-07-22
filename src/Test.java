d15ba9f470f6ed5e83839c152c9d878a4f494a53
Dale <outofthisworld24@hotmail.com>
2016-07-22 12:34:16 +1200

    commit

--- src/Test.java
+++ src/Test.java
@@ -9,8 +9,7 @@ public class Test {
 
     public static void main(String[] args) {
 
-        System.getenv().entrySet().forEach(e -> System.out.println(e.getKey() + " " + e.getValue()));
-        FusionCommandLineParser appCommandLineParser = FusionCommandLineParser.parser();
+
 
         try {
             System.out.println(AndroidSdkUtils.createAVD("/Users/Dale/Library/Android/sdk","DalesAVD","7","default/x86"));
--- src/util/AndroidSdkUtils.java
+++ src/util/AndroidSdkUtils.java
@@ -1,11 +1,10 @@
 package util;
 
-import java.io.*;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 import static util.GlobalConstants.FILE_SEPARATOR;
@@ -23,67 +22,14 @@ public final class AndroidSdkUtils {
     public static final String ANDROID_TOOL = "android";
     public static final String ADB_TOOL = "adb";
     public static final String ANDROID_SDK_ENV_VARIABLE = "ANDROID_SDK_DIR";
-    public static final String LOG_FILE = "/AndroidSdkTools";
+
 
     public static final String CREATE_AVD = "create avd";
 
     public static boolean createAVD(String sdkLocation, String name, String target, String abi) throws IOException {
 
-        Process process = Runtime.getRuntime().exec(buildCommand(sdkLocation, name, target, abi));
-
-        FileWriter bos = new FileWriter(AndroidSdkUtils.class.getResource(LOG_FILE).getPath());
-
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
-
-        int data;
-        int data2;
-
-        InputStream in = process.getInputStream();
-        InputStream in2 = process.getErrorStream();
-        try {
-            while ((data = in.read()) != -1) {
-                System.out.print((char)data);
-                bos.write(data);
-
-                if(in2 != null) {
-                    if ((data2 = in2.read()) != -1)
-                        bos.write(data2);
-
-                    if (data2 != -1 && data == -1) {
-                        in = in2;
-                        in2 = null;
-                    }
-                }
-            }
-            writer.write("no");
-            writer.newLine();
-            writer.flush();
-            writer.close();
-        }catch (Exception e){
-            e.printStackTrace();
-            throw e;
-        }finally {
-            bos.flush();
-            bos.close();
-            process.getErrorStream().close();
-            process.getInputStream().close();
-            process.getOutputStream().close();
-            try {
-                process.waitFor(2000, TimeUnit.MILLISECONDS);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            int exitValue = process.exitValue();
-            process.destroy();
-
-            return exitValue == 0;
-        }
-    }
-
-    private static final String buildCommand(String sdkLocation, String name,String target,String abi){
         StringBuilder commandBuilder = new StringBuilder();
-
-        return commandBuilder.append(sdkLocation)
+        String command = commandBuilder.append(sdkLocation)
                 .append(FILE_SEPARATOR)
                 .append(ANDROID_TOOLS_DIRECTORY)
                 .append(FILE_SEPARATOR)
@@ -93,7 +39,9 @@ public final class AndroidSdkUtils {
                 .append(" --name ").append(name)
                 .append(" --target ").append(target)
                 .append(" --abi ").append(abi).toString();
+        return ProcessUtils.executeCommand(command,"no\n");
     }
+
 
     public static void createAVD(String name, String target, String abi) throws Exception {
         String androidSdkLocation;
